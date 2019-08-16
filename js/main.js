785c87af0bfccc3795c5b0c30276c78ec2a95698
yonesnasaj <yonesnasaj@users.noreply.github.com>
2019-08-16 19:48:52 +0200

    Compleet

---
+++ js/main.js
@@ -0,0 +1,18 @@
+	
+	var name = "John";
+
+	switch(name){
+		
+		case "Mike" :
+			console.log("u bent mike");
+			break;
+		case "John" :
+			console.log("u bent john");
+			break;
+		case "Johnny" :
+			console.log("u bent johnny");
+			break;
+		default :
+			console.log('naam niet gevonden');
+			break;
+	}
